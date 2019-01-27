c079748e42a43c7c76edcd27da06b39df45bc7e4
Sidmar Theodoro <theodorosidmar@gmail.com>
2019-01-27 13:22:03 -0200

    [FEAT] Graceful shutdown

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const server = require('./src/server')
-const { log } = require('./src/utils/logger')
+const { log, logWarn } = require('./src/utils/logger')
 const port = process.env.PORT || 2775
 
 server.listen(port, () => log(`SMPP Server listening on ${port}`))
+
+// Graceful shutdown
+const SIGNALS = {
+  SIGINT: 2,
+  SIGTERM: 15
+}
+
+const shutdownHandler = (signal) => {
+  return () => {
+    logWarn(`Gracefully shutting down the process: #${process.pid}. Exited by signal ${signal}`)
+    server.close()
+  }
+}
+
+Object.keys(SIGNALS).forEach(signal => process.on(signal, shutdownHandler(signal)))
